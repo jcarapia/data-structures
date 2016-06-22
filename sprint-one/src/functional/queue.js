0503fe2ab56282b6c1ceedaed0696480fe69ea70
jcarapia <jcarapia83@gmail.com>
2016-06-22 12:31:17 -0700

    In progress

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,43 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0
+  var count2 = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    count++
+
+    storage[count] = value
   };
 
   someInstance.dequeue = function(){
+    console.log(Object.keys(storage)[0] )
+      //count2++
+    if(count > 0){
+      //return storage[count--]
+      count--;
+    }
+    // if(count === 0){
+    //   console.log(storage[count+1])
+    // return storage[count2-1];
+    // } 
+    // console.log(storage[count2])
+    //return storage[count2-1];
+    for(var key in storage){
+      if(key === count2){
+        count2++
+        return storage[key];
+      }
+    }
+
+
   };
 
   someInstance.size = function(){
+    return count
   };
 
   return someInstance;
 };
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0
   // Implement the methods below
   someInstance.push = function(value){
+    counter ++ ;
+    storage[counter] = value;
+
   };
 
   someInstance.pop = function(){
+    console.log(storage);
+    
+    if(counter > 0){
+      return storage[counter--]
+    }
+    
   };
 
   someInstance.size = function(){
+    return counter; 
   };
 
   return someInstance;
