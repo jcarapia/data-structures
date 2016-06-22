d1830aedfc95ac31116a9f6e4ac3524c574c77ff
jcarapia <jcarapia83@gmail.com>
2016-06-22 15:36:12 -0700

    Passed queu

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,37 +3,27 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var count = 0
+  var count = 0;
   var count2 = 0;
+  var count3 = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
     count++
+    count3++
 
-    storage[count] = value
+    storage[count3] = value
   };
 
   someInstance.dequeue = function(){
-    console.log(Object.keys(storage)[0] )
-      //count2++
+   
     if(count > 0){
-      //return storage[count--]
       count--;
-    }
-    // if(count === 0){
-    //   console.log(storage[count+1])
-    // return storage[count2-1];
-    // } 
-    // console.log(storage[count2])
-    //return storage[count2-1];
-    for(var key in storage){
-      if(key === count2){
-        count2++
-        return storage[key];
-      }
+      count2++;
     }
 
-
+    return storage[count2];
+    
   };
 
   someInstance.size = function(){
