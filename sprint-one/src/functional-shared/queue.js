999ebced8908a3dd3129d61e9b9d8caf103f3c23
jcarapia <jcarapia83@gmail.com>
2016-06-22 16:55:09 -0700

    functional-shared queue is passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+    someInstance.count = 0;
+    someInstance.count2 = 0;
+    someInstance.count3 = 0;
+    someInstance.storage = {};
+    someInstance.enqueue = queueMethods.enqueue;
+    someInstance.dequeue = queueMethods.dequeue;
+    someInstance.size = queueMethods.size;
+  
+  return someInstance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this.count++;
+  this.count3++;
+  this.storage[this.count3] = value;
+}
+queueMethods.dequeue = function(){
+  if (this.count > 0) {
+    this.count--;
+    this.count2++;
+  };
+  return this.storage[this.count2]
+}
 
-
+queueMethods.size = function(){
+  return this.count
+}
