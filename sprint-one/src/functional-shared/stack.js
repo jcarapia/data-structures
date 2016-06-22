03109988b1a78c03c9ac7af9ba56cf835980c3b3
jcarapia <jcarapia83@gmail.com>
2016-06-22 16:32:41 -0700

    functional-shared stack passed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {}
+  someInstance.storage = {}
+  someInstance.counter = 0
+  someInstance.push = stackMethods.push
+  someInstance.pop = stackMethods.pop
+  someInstance.size = stackMethods.size
+
+  return someInstance
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+    
+    this.counter++
+    
+    this.storage[this.counter] = value
+  }
+  stackMethods.pop = function(){
+    if(this.counter > 0){
+     // console.log(this.storage)
+    return this.storage[this.counter--]; 
+    }
 
+
+  }
+  stackMethods.size = function(){
+    return this.counter
+  }
