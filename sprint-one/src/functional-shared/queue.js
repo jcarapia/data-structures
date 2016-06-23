79d10e54d780a2dd13e6ef7f7fc47340f48abb07
jcarapia <jcarapia83@gmail.com>
2016-06-22 17:31:04 -0700

    In progress prototypal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -7,27 +7,30 @@ var Queue = function(){
     someInstance.count2 = 0;
     someInstance.count3 = 0;
     someInstance.storage = {};
-    someInstance.enqueue = queueMethods.enqueue;
-    someInstance.dequeue = queueMethods.dequeue;
-    someInstance.size = queueMethods.size;
+    // someInstance.enqueue = queueMethods.enqueue;
+    // someInstance.dequeue = queueMethods.dequeue;
+    // someInstance.size = queueMethods.size;
+    _.extend(someInstance,queueMethods);
   
   return someInstance;
 };
 
 var queueMethods = {};
+
 queueMethods.enqueue = function(value){
   this.count++;
   this.count3++;
   this.storage[this.count3] = value;
-}
+};
+
 queueMethods.dequeue = function(){
   if (this.count > 0) {
     this.count--;
     this.count2++;
   };
   return this.storage[this.count2]
-}
+};
 
 queueMethods.size = function(){
-  return this.count
-}
+  return this.count;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(Stack.prototype);
+  someInstance.storage = {};
+  someInstance.counter = 0;
+
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+};
+
+Stack.prototype.push = function(value){
+  this.counter++
+  this.storage[this.counter] = value
+};
+
+Stack.prototype.pop = function(){
+  if(this.counter > 0){
+    this.counter--
+  }
+  return this.storage[this.counter+1]
+};
+
+Stack.prototype.size = function(){
+  return this.counter
+};
 
